2bcfad9446a83805afa3f714a0d370dbc31ee088
Anup Pandey <ansh47822@gmail.com>
2026-08-20 18:15:24 +0530

    Updated CV

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1oxJsFGN28BEegVY5i09oP6gMhPieLKXF/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1nT_5_l42eBQGYCNpv_dS3NVpGtK1VHFq/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full text-lg font-bold transition duration-300 transform hover:scale-105"
